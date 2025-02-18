e998897c274dbecf45f3363bf306719b9e72f8df
FarhanGul <FarhanGulKhan29@gmail.com>
2025-02-18 17:29:41 +0500

    The pet shop UI should disappear when player moves away

--- Assets/_Project/Scripts/PetShop.lua
+++ Assets/_Project/Scripts/PetShop.lua
@@ -6,6 +6,7 @@ local save = require("SaveManager")
 
 local ve
 local petFoodCost = 5
+local isUiActive
 
 function self:Awake()
     self.gameObject:GetComponent(TapHandler).Tapped:Connect(function() 
@@ -13,7 +14,14 @@ function self:Awake()
     end)
 end
 
+function self:Update()
+    if(isUiActive and client.localPlayer.character.isMoving)then
+        Hide()
+    end
+end
+
 function Show()
+    isUiActive = true
     ve = VisualElement.new()
     local label = UILabel.new()
     label:SetPrelocalizedText("Welcome to the pet shop")
@@ -24,15 +32,18 @@ function Show()
     end,true))
     ve:Add(uiComponents.TextButton("Buy Pet Food ( "..petFoodCost.." )",function() 
         if(save.coins >= petFoodCost) then
-            Hide()
             events.InvokeEvent(events.boughtPetFood)
             save.SetCoins(save.coins - petFoodCost )
             print("Bought pet food")
         end
     end,save.coins >= petFoodCost))
+    ve:Add(uiComponents.TextButton("Close",function() 
+        Hide()
+    end,true))
     uiController.Add(ve)
 end
 
 function Hide()
+    isUiActive = false
     ve:RemoveFromHierarchy()
 end
